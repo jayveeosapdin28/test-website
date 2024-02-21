08ec0b86ac588ad3a13ae5e60685d9db1cec5256
Jayvee Osapdin <34233093+jayveeosapdin28@users.noreply.github.com>
2024-02-21 08:24:32 +0800

    Initial setup

---
+++ test-website/layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import Topnav from "~/components/navigators/topnav.vue";
+import Botnav from "~/components/navigators/botnav.vue";
+</script>
+
+<template>
+<div>
+  <topnav/>
+  <div class="container mx-auto">
+    <slot/>
+  </div>
+  <botnav/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ test-website/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-blue-500">ss</div>
+</template>
+
+<style scoped>
+
+</style>
