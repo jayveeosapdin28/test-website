d7946cec01290812b248b0f3e7babbb52d3ddd69
Jayvee Osapdin <34233093+jayveeosapdin28@users.noreply.github.com>
2024-02-21 11:02:23 +0800

    Add top nav dropdown for mobile

---
+++ test-website/components/navigators/partials/dropdown.vue
@@ -0,0 +1,76 @@
+<template>
+  <Transition name="nested">
+    <div
+        class="fixed top-0 left-0 w-full h-full bg-blue-100 bg-opacity-60 flex justify-center outer z-50"
+        v-if="internalModel"
+        @click="internalModel = false"
+    >
+      <div class="w-full p-8  relative top-8 inner">
+        <div class="bg-white rounded-xl shadow-lg p-6">
+         <ul class="flex flex-col gap-4">
+           <li v-for="link in links" :key="`${link.label}-link`">
+             <NuxtLink :to="link.to">{{link.label}}</NuxtLink>
+           </li>
+           <li>
+             <hr class="text-neutral-200">
+           </li>
+           <li>
+             <NuxtLink to="/login">Login</NuxtLink>
+           </li>
+         </ul>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+import {computed, defineProps, defineEmits} from 'vue';
+
+const props = defineProps({
+  modelValue: Boolean,
+  links: Object
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const internalModel = computed({
+  set: (val) => {
+    emit('update:modelValue', val)
+  },
+  get: () => props.modelValue
+});
+
+</script>
+
+<style scoped>
+
+.nested-enter-active, .nested-leave-active {
+  transition: all 0.2s ease-in-out;
+}
+
+.nested-leave-active {
+  transition-delay: 0.25s;
+}
+
+.nested-enter-from,
+.nested-leave-to {
+  opacity: 0;
+}
+
+
+.nested-enter-active .inner,
+.nested-leave-active .inner {
+  transition: all 0.2s ease-in-out;
+}
+
+.nested-enter-active .inner {
+  transition: all 0.2s ease-in-out;
+}
+
+.nested-enter-from .inner,
+.nested-leave-to .inner {
+  transform: translateY(-30px);
+  opacity: 0.001;
+}
+</style>
--- test-website/components/navigators/topnav.vue
+++ test-website/components/navigators/topnav.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts">
+<script setup>
 
+import Dropdown from "~/components/navigators/partials/dropdown.vue";
+
+const showingNavigationDropdown = ref(false);
+
+const links = [
+  {label: 'Features', to: '/'},
+  {label: 'Testimonial', to: '/'},
+  {label: 'Pricing', to: '/'},
+]
 </script>
 
 <template>
@@ -11,19 +20,13 @@
         <!--links-->
         <div class="hidden flex-grow xl:flex justify-between items-center">
           <ul class="flex gap-4">
-            <li>
-              <NuxtLink to="/">Features</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/">Testimonial</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/">Pricing</NuxtLink>
+            <li v-for="link in links" :key="`${link.label}-link`">
+              <NuxtLink :to="link.to">{{link.label}}</NuxtLink>
             </li>
           </ul>
           <ul class="flex gap-4 items-center">
             <li>
-              <NuxtLink to="/">Login</NuxtLink>
+              <NuxtLink to="/login">Login</NuxtLink>
             </li>
           </ul>
         </div>
@@ -32,24 +35,32 @@
         <!--try button-->
         <button class="btn bg-neutral-800 text-white px-3 py-1">Try for free</button>
         <!--hamburger-->
-        <div class="xl:hidden">
-          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path
 
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-            />
-            <!--          <path-->
-            <!--              stroke-linecap="round"-->
-            <!--              stroke-linejoin="round"-->
-            <!--              stroke-width="2"-->
-            <!--              d="M6 18L18 6M6 6l12 12"-->
-            <!--          />-->
-          </svg>
+        <div class="xl:hidden">
+          <button
+              @click="showingNavigationDropdown = !showingNavigationDropdown"
+              class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+          >
+            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <path
+                  :class="{hidden: showingNavigationDropdown,'inline-flex': !showingNavigationDropdown}"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+              />
+              <path
+                  :class="{hidden: !showingNavigationDropdown, 'inline-flex': showingNavigationDropdown,                    }"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </div>
       </div>
+      <Dropdown v-model="showingNavigationDropdown" :links="links"/>
     </div>
   </nav>
 </template>
