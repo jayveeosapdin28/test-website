e4938ad94f86ac9738eea58609ff15ba4ea8f423
Jayvee Osapdin <34233093+jayveeosapdin28@users.noreply.github.com>
2024-02-22 15:24:36 +0800

    update secondary feature

--- test-website/components/sections/feature/secondaryFeature.vue
+++ test-website/components/sections/feature/secondaryFeature.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+const { data } = await useFetch('/api/feature')
 </script>
 
 <template>
@@ -12,6 +12,19 @@
       <p class="text-lg">
         HireData is designed to streamline your recruitment operations, freeing you to focus on securing top talent
       </p>
+    </div>
+    <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0 mt-8">
+      <template v-for="item in data">
+        <div>
+          <div class="flex justify-center">
+            <div class="flex  justify-center items-center mb-4 w-20 h-20 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+              <img :src="`/_nuxt/assets/img/secondary_feature/${item.icon}.png`" :alt="item.icon" class="w-full h-full object-contain">
+            </div>
+          </div>
+          <h3 class="mb-2 text-xl font-bold text-center dark:text-white">{{item.title}}</h3>
+          <p class="text-gray-500 dark:text-gray-400 text-center">{{item.description}}</p>
+        </div>
+      </template>
     </div>
   </div>
 </section>
---
+++ test-website/server/api/feature.js
@@ -0,0 +1,20 @@
+export default defineEventHandler(() => [
+    {
+        icon: 'analytic',
+        tag: 'Advanced Reporting',
+        title: 'Stay on top of your hiring process.',
+        description: 'With our real-time reporting features, you can track progress, measure success, and refine your strategy.',
+    },
+    {
+        icon: 'horizontal',
+        tag: 'Candidate Management',
+        title: 'Never lose track of your talent pool.',
+        description: 'Our robust tracking system ensures you always know where every candidate stands in your recruitment pipeline.',
+    },
+    {
+        icon: 'person',
+        tag: 'Integrated Contacts',
+        title: 'Organize your recruitment network.',
+        description: 'Consolidate all your candidate details, hiring managers, and recruitment documentation in one easy-to-access place.',
+    },
+])
